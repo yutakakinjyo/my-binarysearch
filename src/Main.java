32b48d78682914208445cd0ef8c6bc06d6b13f15
yutakakinjyo <yutakakinjyo@gmail.com>
2015-04-29 23:05:42 +0900

    バイナリサーチ用のインスタンスを作成

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,11 @@ public class Main {
 	private static final int TARGET = 12;
 
 	public static void main(String[] args) {
-
+		
 		// サンプル配列の生成
 		int[] sample_data = {3, 4, 5, 10, 14, 12, 25, 30, 49, 100};
+		
+		BinarySearchArray array =  new BinarySearchArray(sample_data);
 
 		// ソート
 		Arrays.sort(sample_data);
