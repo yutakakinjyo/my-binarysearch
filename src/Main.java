015e8658055590eae833b4750defa891b629b859
yutakakinjyo <yutakakinjyo@gmail.com>
2015-04-29 23:20:15 +0900

    バイナリサーチのメソッドを、バイナリサーチ用のクラスのメソッドに変更し、元のメソッドは削除

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-
-import java.util.Arrays;
-
 public class Main {
 
 	private static final int TARGET = 12;
@@ -17,7 +14,7 @@ public class Main {
 		System.out.println(array);
 
 		// 二分探索し、結果を返す
-		boolean isPresence = BinarySearch(array.array, TARGET);
+		boolean isPresence = array.BinarySearch(TARGET);
 
 		// 結果を出力する
 		if(isPresence) {
@@ -26,25 +23,6 @@ public class Main {
 			System.out.println(TARGET + "はありませんでした。");
 		}
 
-	}
-
-	private static boolean BinarySearch(int[] array, int target) { 
-
-		int index = array.length/2;
-		int center = array[index]; 
-
-		// 中央値を判定
-		if (center == target) { // 中央値と値が同じ場合
-			return true;
-		} else if(array.length == 1) { // 見つからなかった場合 
-			return false;
-		} else if (center < target) { // 中央値より値が大きい場合
-			array = Arrays.copyOfRange(array, 0, index);
-			return BinarySearch(array, target);
-		} else { // 中央値より値が小さい場合
-			array = Arrays.copyOfRange(array, index, array.length);
-			return BinarySearch(array, target);
-		}
-	}				
+	}	
 }
 
