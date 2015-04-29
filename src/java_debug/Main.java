ce613958d20828d2619870921813e8b92dc07a7d
yutakakinjyo <yutakakinjyo@gmail.com>
2015-04-29 19:28:18 +0900

    見つからなかった場合の条件式を正しく設定 配列の長さが1で、探索対象の値と中央値の値が違う場合には、見つからなかったことになる。

--- src/java_debug/Main.java
+++ src/java_debug/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		// 二分探索を実装する		
 
 		// サンプル配列の生成
-		int[] sample_data = {3, 4, 12, 5, 10, 14, 25, 30, 49, 100};
+		int[] sample_data = {3, 4, 5, 10, 14, 12, 25, 30, 49, 100};
 
 		// ソート
 		Arrays.sort(sample_data);
@@ -25,20 +25,22 @@ public class Main {
 		
 	}
 
-	private static boolean BinarySearch(int[] array, int target) {
-		// 中央値を判定
-		int center = array[(array.length/2)]; 
+	private static boolean BinarySearch(int[] array, int target) { 
+		
+		int index = array.length/2;
+		int center = array[index]; 
 
-		if (center > target) { // 中央値より値が大きい場合
-			array = Arrays.copyOfRange(array, 0, array.length/2);
-			return BinarySearch(array, target);
-		} else if(center < target) { // 中央値より値が小さい場合
-			array = Arrays.copyOfRange(array, array.length/2, array.length);
-			return BinarySearch(array, target);
-		} else if(center == target) { // 中央値と値が同じ場合
+		// 中央値を判定
+		if (center == target) { // 中央値と値が同じ場合
 			return true;
-		} else { // 見つからなかった場合
+		} else if(array.length == 1) { // 見つからなかった場合 
 			return false;
-		}
+		} else if (center > target) { // 中央値より値が大きい場合
+			array = Arrays.copyOfRange(array, 0, index);
+			return BinarySearch(array, target);
+		} else // 中央値より値が小さい場合
+			array = Arrays.copyOfRange(array, index, array.length);
+			return BinarySearch(array, target);
+		}				
 	}
-}
+
