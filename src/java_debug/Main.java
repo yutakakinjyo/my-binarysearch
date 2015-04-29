92e2928ec93feef3aefc415beba1ceda3d82b823
yutakakinjyo <yutakakinjyo@gmail.com>
2015-04-29 18:52:29 +0900

    探索結果を表示

--- src/java_debug/Main.java
+++ src/java_debug/Main.java
@@ -7,16 +7,22 @@ public class Main {
 		// 二分探索を実装する		
 
 		// サンプル配列の生成
-		int[] sample_data = {3, 4, 5, 10, 12, 14, 25, 30, 49, 100};
+		int[] sample_data = {3, 4, 12, 5, 10, 14, 25, 30, 49, 100};
 
 		// ソート
 		Arrays.sort(sample_data);
 		System.out.println(Arrays.toString(sample_data));
 
 		// 二分探索し、結果を返す
-		BinarySearch(sample_data, 12);
+		boolean isPresence = BinarySearch(sample_data, 12);
 
 		// 結果を出力する
+		if(isPresence) {
+			System.out.println("12はありました。");
+		} else {
+			System.out.println("12はありませんでした。");
+		}
+		
 	}
 
 	private static boolean BinarySearch(int[] array, int target) {
@@ -24,27 +30,15 @@ public class Main {
 		int center = array[(array.length/2)]; 
 
 		if (center > target) { // 中央値より値が大きい場合
-			
 			array = Arrays.copyOfRange(array, 0, array.length/2);
 			return BinarySearch(array, target);
-			
 		} else if(center < target) { // 中央値より値が小さい場合
-
-			Arrays.copyOfRange(array, array.length/2, array.length);
+			array = Arrays.copyOfRange(array, array.length/2, array.length);
 			return BinarySearch(array, target);
-			
 		} else if(center == target) { // 中央値と値が同じ場合
-			
 			return true;
-			
 		} else { // 見つからなかった場合
-
 			return false;
-			
 		}
-		
-
 	}
-	
-	
 }
