ba84e6653ea35a8487047f6ee253d7bfd72d9a04
yutakakinjyo <yutakakinjyo@gmail.com>
2015-04-29 18:03:33 +0900

    - サンプル配列の生成 - ソート
    を実装

--- src/java_debug/Main.java
+++ src/java_debug/Main.java
@@ -1,12 +1,16 @@
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// 二分探索を実装する
+		// 二分探索を実装する		
 
 		// サンプル配列の生成
+		int[] sample_data = {3, 4, 5, 10, 12, 14, 25, 30, 49, 100};
 
 		// ソート
+		Arrays.sort(sample_data);
 
 		// 二分探索し、結果を返す
 
